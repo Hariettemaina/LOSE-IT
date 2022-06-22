ad560b27a5bebdbbd0a6715e25c8ae008c2f1bf5
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-22 13:01:56 +0300

    ft:function to register users

--- lose/views.py
+++ lose/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import SelectFoodForm,AddFoodForm,CreateUserForm,ProfileForm
 from .models import *
+from datetime import timedelta
+from django.utils import timezone
 from datetime import date
 from datetime import datetime
 from .filters import FoodFilter
-
-
 
 
 
@@ -41,7 +44,46 @@ def HomepageView(request):
 	'over_calorie' : over_calorie,
 	'food_selected_today':all_food_today
 	}
-	   
+
+#signup page
+def RegisterPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request,"Account was created for "+ user)
+				return redirect('login')
+
+		context = {'form':form}
+		return render(request,'register.html',context)	   
     
-    
+
+
+#login page
+def LoginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			user = authenticate(request,username=username,password=password)
+			if user is not None:
+				login(request,user)
+				return redirect('home')
+			else:
+				messages.info(request,'Username or password is incorrect')
+		context = {}
+		return render(request,'login.html',context)
+
+#logout page
+def LogOutPage(request):
+	logout(request)
+	return redirect('login')    
     
