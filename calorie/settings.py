77f57f487661d202ea8cb7c799f33c570e5872b2
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-22 08:22:35 +0300

    ft:psycopg2

--- calorie/settings.py
+++ calorie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +77,15 @@ WSGI_APPLICATION = 'calorie.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': 'calorie',
+           'USER': 'carolmaina',
+           'PASSWORD': 'ethan2017',
+           
+       }
+       
+   }
 
 
 # Password validation
@@ -101,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
