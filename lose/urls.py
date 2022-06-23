7db4d3bdad8a981882e8f6ad5fc30f2dc5390562
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-23 22:27:43 +0300

    define routes

--- lose/urls.py
+++ lose/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     path('login/',LoginPage,name='login'),
 	path('logout/',LogOutPage,name='logout'),
     path('register/',RegisterPage,name='register'),
+    path('add_food/',add_food,name='add_food'),
+    path('select_food/',select_food,name='select_food'),
+    path('update_food/<str:pk>/',update_food,name='update_food'),
+    path('delete_food/<str:pk>/',delete_food,name='delete_food'),
+    path('profile/',ProfilePage,name='profile'),
 ]
