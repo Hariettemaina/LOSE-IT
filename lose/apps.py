b5c00678b2fbefcb0cdbdd0532e89e8c9c062cb5
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-06-23 22:25:05 +0300

    register signals

--- lose/apps.py
+++ lose/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class LoseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'lose'
+    
+    def ready(self): #new
+        import lose.signals #new
